35453233cc956051408beeac5d7f69c1869b949f
VinuthnaTaduru <153432214+VinuthnaTaduru@users.noreply.github.com>
2026-04-08 12:33:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/about', (req, res) => {
+  res.send('about us');
+});
  
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
